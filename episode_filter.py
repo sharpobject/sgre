5273d2ed2502b91e0a00c47c3ac53554fdefc00f
Robert Burke <sharpobject@gmail.com>
2014-01-19 15:17:03 -0600

    mess with ep filter

--- episode_filter.py
+++ episode_filter.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 import json
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='list skills and card IDs by episode.')
+parser.add_argument('episodes', metavar='E', type=str, nargs='+',
+                      help='an episode m9m9m9')
+parser.add_argument('--id-only', action='store_true',
+                     help='print the ids only')
+parser.add_argument('--skill-only', action='store_true',
+                     help='print the skills only')
+args = vars(parser.parse_args())
 
 swogi = json.loads(open('swogi.json','r').read())
-if len(sys.argv) == 1:
+if len(args["episodes"]) == 0:
 	print "please add an episode number as an argument.  valid eps are: [u'EP0', u'EP2', u'EP11', \
 	u'EP10', u'EX3', u'UE1', u'EX5', u'EP17', u'EP6', u'EP19', u'UE5', u'EX2', u'EP1', u'EP5', u'EP3', \
 	u'EX4', u'EP13', u'EP18', u'EP16', u'EP8', u'EP12', u'UE4', u'UE2', u'EP14', u'UE3', u'EP15', \
 	u'EPM', u'EP9', u'EX6', u'EX1', u'EP7', u'EP4']"
 	sys.exit()
-ep = sys.argv[1]
+eps = set(args["episodes"])
+id_only = args["id_only"]
+skill_only = args["skill_only"]
 
 def display_info(card):
   if card["type"]=="Follower":
@@ -24,7 +35,7 @@ skills = set()
 id2card = swogi['id_to_card']
 
 for key in id2card.keys():
-	if id2card[key]['episode'] == ep:
+	if id2card[key]['episode'] in eps:
 		if id2card[key]['type'] == 'Character':
 			characters.append(id2card[key])
 		elif id2card[key]['type'] == 'Spell':
@@ -37,16 +48,22 @@ characters.sort(key=lambda card: card['id'])
 spells.sort(key=lambda card: card['id'])
 followers.sort(key=lambda card: card['id'])
 
-print "Showing cards from episode", ep
-print '\n', "characters:".ljust(48), "skills:"
-for character in characters:
-	display_info(character)
-print '\n', "spells:".ljust(48), "skills:"
-for spell in spells:
-	display_info(spell)
-print '\n', "followers:".ljust(48), "skills:"
-for follower in followers:
-	display_info(follower)
-print '\n'
-for skill in sorted(list(skills)):
-  print skill
+if not (skill_only or id_only):
+  print "Showing cards from episode", eps
+  print '\n', "characters:".ljust(48), "skills:"
+  for character in characters:
+    display_info(character)
+  print '\n', "spells:".ljust(48), "skills:"
+  for spell in spells:
+    display_info(spell)
+  print '\n', "followers:".ljust(48), "skills:"
+  for follower in followers:
+    display_info(follower)
+
+if skill_only:
+  for skill in sorted(list(skills)):
+    print skill
+
+if id_only:
+  for card in characters+spells+(followers if not skill_only else []):
+    print card["id"]
