1af8f028948aebcd77b761f18d52dccd2f4d1c08
Robert Burke <sharpobject@gmail.com>
2014-06-08 18:17:35 -0700

    fix dard

--- server.lua
+++ server.lua
@@ -845,7 +845,7 @@ function Connection:try_xmute(msg)
   end
   for _,deck in pairs(data.decks) do
     if deck[from_card_id] and
-        data.collection[from_dard_id] - deck[from_card_id] <
+        data.collection[from_card_id] - deck[from_card_id] <
         to_card_number * multiplier then
       return false --stuff is being used in decks
     end
