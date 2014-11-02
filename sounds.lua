76cab95e5f3521e84e006b33d8052a067ec17be9
Robert Burke <sharpobject@gmail.com>
2014-11-02 01:41:24 -0700

    move bgm

--- sounds.lua
+++ sounds.lua
@@ -1,11 +1,11 @@
 bgm = nil
 
 local bgm_table = {}
-bgm_table["lobby"] = love.audio.newSource("sg_assets/bgm/SwordGirls_Waiting_Room.mp3")
-bgm_table["dungeon"] = love.audio.newSource("sg_assets/bgm/4_sg_bgm_dugeon.mp3")
-bgm_table["rewards"] = love.audio.newSource("sg_assets/bgm/3_sg_bgm_result.mp3")
-bgm_table["fight"] = love.audio.newSource("sg_assets/bgm/2_sg_bgm_vs_02.mp3")
-bgm_table["other_main"] = love.audio.newSource("sg_assets/bgm/5_sg_bgm_main_2.mp3")
+bgm_table["lobby"] = love.audio.newSource("sg_bgm/SwordGirls_Waiting_Room.mp3")
+bgm_table["dungeon"] = love.audio.newSource("sg_bgm/4_sg_bgm_dugeon.mp3")
+bgm_table["rewards"] = love.audio.newSource("sg_bgm/3_sg_bgm_result.mp3")
+bgm_table["fight"] = love.audio.newSource("sg_bgm/2_sg_bgm_vs_02.mp3")
+bgm_table["other_main"] = love.audio.newSource("sg_bgm/5_sg_bgm_main_2.mp3")
 
 for state, source in pairs(bgm_table) do
     source:setLooping(true)
